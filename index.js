3e8608ea7bf67c2daad7b106dca0891ac8c82a34
Adrian Samarripas <asamarripas@gmail.com>
2018-05-10 18:48:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,25 @@ function takeANumber(katz, name){
   return `Welcome, ${name}. You are number ${katz.length} in line.`;
 }
 
-function nowServing(){
+
+function takeANumber(katz){
+  
+}
+
+// takeANumber(katz) => "Welcome. You're ticket number #1" [1]
+// takeANumber(katz) => "Welcome. You're ticket number #2" [1,2]
+
+// nowServing(katz) => "Currently serving: #1" [2]
+// nowServing(katz) => "Currently serving: #2" []
+
+// takeANumber(katz) => "Welcome. You're ticket number #3" [3]
+
+function nowServing(katz){
   if (katz.length>0){
-    console.log(`Currently serving )
+    console.log(`Currently serving`)
   }
+}
+
+function currentLine(katzDelLine){
+  
 }
